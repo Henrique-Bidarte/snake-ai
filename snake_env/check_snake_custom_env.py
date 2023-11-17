b6b8e219a0ff4a003415672f1015a44e73c97143
Henrique-Bidarte <henriqueb1208@gmail.com>
2023-11-17 14:35:31 -0300

    Solving imports

--- snake_env/check_snake_custom_env.py
+++ snake_env/check_snake_custom_env.py
@@ -1,5 +1,5 @@
 from stable_baselines3.common.env_checker import check_env
-from snake_game_custom_env import SnakeCustomEnv
+from snake_custom_env import SnakeCustomEnv
 
 env = SnakeCustomEnv()
 check_env(env)
